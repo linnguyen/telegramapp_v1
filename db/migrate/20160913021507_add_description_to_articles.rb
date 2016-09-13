edb63a370ebbe9da184824dab0781ba23ecd0f0a
linguyen <nguyenvanlin01041994@gmail.com>
2016-09-13 10:14:59 +0700

    Create article table modle and add validation

---
+++ db/migrate/20160913021507_add_description_to_articles.rb
@@ -0,0 +1,10 @@
+class AddDescriptionToArticles < ActiveRecord::Migration
+  def change
+    add_column :articles, :description,:text
+  	add_column :articles, :created_at, :datetime
+  	add_column :articles, :update_at, :datetime
+  	add_column :articles, :status, :datetime
+
+
+  end
+end
